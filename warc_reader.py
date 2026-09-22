18c44624b0b612eb439beff15d5beb48b4ff0d9a
SamerM0 <samer.mohamed600012@gmail.com>
2026-09-22 17:42:20 +0300

    Implement Chunking

--- warc_reader.py
+++ warc_reader.py
@@ -1,11 +1,18 @@
 from warcio.archiveiterator import ArchiveIterator
 from bs4 import BeautifulSoup
 
+
 with open("crawl/data.warc.gz","rb") as stream:
     for record in ArchiveIterator(stream):
         if record.rec_type == 'response':
             html = record.content_stream().read()
             soup = BeautifulSoup(html,"html.parser")
-            title = soup.find("title")
-            if title:
-                print(title.text)
+            body = soup.find("body")
+            if body:
+                body = body.text
+                words = body.split()
+                chunks = []
+                for i in range(0, len(words), 100):
+                    chunk = words[i:i+120]
+                    chunk_joined = " ".join(chunk)
+                    chunks.append(chunk_joined)
